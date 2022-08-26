db71e1ca8f3c9ae08f8a8b511916536a9edadf4a
hardstein <89636138+hardstein@users.noreply.github.com>
2022-08-26 11:58:19 +0200

    Removed unnecessary files, annd made a folder structure.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Registration from "./components/Registration";
+
+function App() {
+  return (
+    <>
+      <h1>Hello!</h1>
+      <Registration />
+    </>
+  );
+}
+
+export default App;
