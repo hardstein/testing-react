3cb2351e2cde2b8e5ac63f16fa65a3b052b3a358
hardstein <89636138+hardstein@users.noreply.github.com>
2022-08-30 16:03:37 +0200

    Added some tests according to alternative 3. Styled the application.

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -1,49 +1,70 @@
 import { useState } from "react";
 function Registration() {
-  const [name, setName] = useState("");
-  const [isChecked, setIsChecked] = useState(false);
+  const initialValues = { name: "", age: "", member: false };
+  const [formValues, setFormValues] = useState(initialValues);
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
 
-  const handleCheckboxChange = () => {
-    setIsChecked(!isChecked);
-  };
-
-  const handleInputChange = (event) => {
-    const target = event.target.value;
-    setName(target);
+  const validate = (values) => {
+    const errors = {};
+    if (!values.name) {
+      errors.name = "Name is required!";
+    }
+    if (!values.age) {
+      errors.age = "Age is required!";
+    }
+    return errors;
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    return name;
-    // alert(`Registration as user: ${name} is complete`);
+    setFormErrors(validate(formValues));
+    setIsSubmit(true);
   };
 
   return (
     <>
       <h1>Registration</h1>
-      {/* <p>User: {name}</p> */}
       <form onSubmit={handleSubmit}>
+        <h1 id="isSubmitText">{isSubmit && Object.keys(formErrors).length === 0 ? "Registration complete." : null}</h1>
         <label>
           Username:
           <input
             name="username"
             type="text"
-            value={name || ""}
-            onChange={handleInputChange}
+            value={formValues.name}
+            // Valiation on characters is done with onChange
+            onChange={(e) =>
+              setFormValues({
+                ...formValues,
+                name: e.target.value.replace(/[^A-Za-z]/g, ""),
+              })
+            }
           />
         </label>
+        <p className="errorText" id="errorName">{formErrors.name}</p>
+        <label>
+          Age:
+          <input
+            name="age"
+            type="number"
+            value={formValues.age}
+            onChange={(e) =>
+              setFormValues({ ...formValues, age: e.target.value })
+            }
+          />
+        </label>
+        <p className="errorText">{formErrors.age}</p>
         <label>
           Member:
           <input
             type="checkbox"
-            checked={isChecked}
-            onChange={handleCheckboxChange}
+            checked={formValues.member}
+            onChange={(e) =>
+              setFormValues({ ...formValues, member: !formValues.member })
+            }
           />
         </label>
-        <p>
-          {name.length > 0 ? name + " - " : null}
-          {isChecked ? "member" : "not"}
-        </p>
         <input name="inpSubmit" type="submit" value={"Register"} />
       </form>
     </>
--- src/components/Registration.test.jsx
+++ src/components/Registration.test.jsx
@@ -1,24 +1,117 @@
-import { fireEvent, render } from "@testing-library/react";
-// import { shallow } from "enzyme";
+import { screen, fireEvent, render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Registration from "./Registration";
 
-test("Should have no value/state to begin with.", () => {
-  render(<Registration />);
+// https://testing-playground.com/
 
-  const content = document.querySelector("input");
-  expect(content.value).toBe("");
-});
-
-test("renders the landing page", () => {
+test("Renders component registration page", () => {
   render(<Registration />);
 });
 
-test("CheckboxWithLabel changes the text after click", () => {
-  const { getByLabelText } = render(<Registration />);
-  const text = document.querySelector("p");
-  const checkbox = getByLabelText("Member:");
-
-  fireEvent.click(checkbox);
-
-  expect(text.textContent).toBe("member");
+test("Input for name should be empty to begin with.", () => {
+  render(<Registration />);
+  expect(getNameInput().value).toBe("");
 });
+
+test("Member checkbox changes to checked when clicked.", () => {
+  render(<Registration />);
+
+  expect(getMemberCheckbox().checked).toEqual(false);
+
+  fireEvent.click(getMemberCheckbox());
+
+  expect(getMemberCheckbox().checked).toEqual(true);
+});
+
+test("Name will update correctly with value from input.", () => {
+  render(<Registration />);
+
+  expect(getNameInput().value).toBe("");
+
+  userEvent.type(getNameInput(), "Per");
+
+  expect(getNameInput().value).toBe("Per");
+});
+
+test("Name input value will not update with numbers.", () => {
+  render(<Registration />);
+
+  expect(getNameInput().value).toBe("");
+
+  userEvent.type(getNameInput(), "1Per1");
+
+  expect(getNameInput().value).toBe("Per");
+});
+
+test("Age will update correctly with value from input.", () => {
+  render(<Registration />);
+
+  fireEvent.change(getAgeInput(), {
+    target: { value: 12 },
+  });
+
+  expect(Number(getAgeInput().value)).toBe(12);
+});
+
+test("Age input value will only accept numbers.", () => {
+  render(<Registration />);
+
+  userEvent.type(getAgeInput(), "12letter");
+
+  expect(Number(getAgeInput().value)).toBe(0);
+});
+
+test("Form show error text when input is missing and submit is clicked.", () => {
+  render(<Registration />);
+
+  expect(getNameInput().value).toBe("");
+
+  userEvent.click(getRegisterButton());
+
+  expect(getErrorTextName().innerHTML).toBe("Name is required!");
+});
+
+test("Form show registration complete when submit is clicked with correct input.", () => {
+  render(<Registration />);
+
+  fireEvent.change(getAgeInput(), {
+    target: { value: 12 },
+  });
+
+  userEvent.type(getNameInput(), "Per");
+
+  userEvent.click(getRegisterButton());
+  expect(getHeadingSubmit().innerHTML).toBe("Registration complete.");
+});
+
+function getNameInput() {
+  return screen.getByRole("textbox", {
+    name: /username:/i,
+  });
+}
+
+function getAgeInput() {
+  return screen.getByRole("spinbutton", {
+    name: /age:/i,
+  });
+}
+
+function getMemberCheckbox() {
+  return screen.getByRole("checkbox", {
+    name: /member:/i,
+  });
+}
+
+function getHeadingSubmit() {
+  return document.querySelector("#isSubmitText");
+}
+
+function getErrorTextName() {
+  return document.querySelector("#errorName");
+}
+
+function getRegisterButton() {
+  return screen.getByRole("button", {
+    name: /register/i,
+  });
+}
